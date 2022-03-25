edf6a4600a69ace511249b2e569c24003e2ffce1
Blzping <pattapon37459@gmail.com>
2022-03-25 13:23:41 +0700

    Create Exercise4_Pattapon_S.py
    Exercise4

---
+++ Exercise4_Pattapon_S.py
@@ -0,0 +1,9 @@
+English = 70
+Business = 65
+ComputerSystems = 72
+Programming = 69
+print("--- Your Score ---")
+print("Foundation English",":" , English)
+print("General Business :", Business)
+print("Introduction to Computer Systems :", ComputerSystems)
+print("Computer Programming :", Programming)
